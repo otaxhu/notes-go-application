798cdb5e01bacc95c8141b5567d488d3988d919b
oscar-jesus <oscarperniamoreno@gmail.com>
2023-02-18 16:52:56 -0400

    id parsed to string so it can be a uuid

--- models/note.go
+++ models/note.go
@@ -7,7 +7,7 @@ import (
 )
 
 type Note struct {
-	ID          uint           `gorm:"primaryKey" json:"id"`
+	ID          string         `gorm:"primaryKey" json:"id"`
 	CreatedAt   time.Time      `json:"created_at"`
 	UpdatedAt   time.Time      `json:"updated_at"`
 	DeletedAt   gorm.DeletedAt `gorm:"index" json:"deleted_at,omitempty"`
