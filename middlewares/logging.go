69dd4d659b99404308458fc7ea238acb40b41bd4
oscar-jesus <oscarperniamoreno@gmail.com>
2023-02-17 22:34:46 -0400

    logging middleware created and added to the Router

--- middlewares/logging.go
+++ middlewares/logging.go
@@ -1,1 +1,14 @@
 package middlewares
+
+import (
+	"log"
+	"net/http"
+)
+
+func Logging(f http.Handler) http.Handler {
+	return http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			log.Printf("%s %s", r.Method, r.URL.Path)
+			f.ServeHTTP(w, r)
+		})
+}
