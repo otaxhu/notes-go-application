69ed2785d291620a0b210e2a5be3b1dcbc4311ed
oscar-jesus <oscarperniamoreno@gmail.com>
2023-02-18 16:52:27 -0400

    endpoints changed, endpoint /api/users created

--- router/router.go
+++ router/router.go
@@ -17,9 +17,10 @@ func InitializeRouter() {
 	r.HandleFunc("/api", controllers.HandleHome).Methods("GET")
 	r.HandleFunc("/api/notes", controllers.GetNotes).Methods("GET")
 	r.HandleFunc("/api/notes/{id}", controllers.GetNoteByID).Methods("GET")
-	r.HandleFunc("/api/notes/create", controllers.CreateNote).Methods("POST")
-	r.HandleFunc("/api/notes/{id}/update", controllers.UpdateNoteByID).Methods("PUT")
-	r.HandleFunc("/api/notes/{id}/delete", controllers.DeleteNoteByID).Methods("DELETE")
+	r.HandleFunc("/api/notes", controllers.CreateNote).Methods("POST")
+	r.HandleFunc("/api/notes/{id}", controllers.UpdateNoteByID).Methods("PUT")
+	r.HandleFunc("/api/notes/{id}", controllers.DeleteNoteByID).Methods("DELETE")
+	r.HandleFunc("/api/users", controllers.CreateUser).Methods("POST")
 
 	log.Fatal(http.ListenAndServe(":3000", r))
 }
