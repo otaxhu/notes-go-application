975ea3461b31469e4dbe4c52740b49ede94a710a
oscar-jesus <oscarperniamoreno@gmail.com>
2023-02-17 22:33:18 -0400

    not using anymore gorm.Model, json items lowercased

--- models/note.go
+++ models/note.go
@@ -1,9 +1,16 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Note struct {
-	gorm.Model
-	Title       string `json:"title" gorm:"not null"`
-	Description string `json:"description" gorm:"not null"`
+	ID          uint           `gorm:"primaryKey" json:"id"`
+	CreatedAt   time.Time      `json:"created_at"`
+	UpdatedAt   time.Time      `json:"updated_at"`
+	DeletedAt   gorm.DeletedAt `gorm:"index" json:"deleted_at,omitempty"`
+	Title       string         `json:"title" gorm:"not null"`
+	Description string         `json:"description" gorm:"not null"`
 }
