6a922b8aeab716d945548c56d76a991f1d89c045
oscar-jesus <oscarperniamoreno@gmail.com>
2023-02-17 22:35:35 -0400

    endpoints with CRUD operations added with its controllers

--- router/router.go
+++ router/router.go
@@ -1,20 +1,25 @@
 package router
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 	"github.com/otaxhu/notes-go-application/controllers"
+	"github.com/otaxhu/notes-go-application/middlewares"
 )
 
 func InitializeRouter() {
 	r := mux.NewRouter()
 
-	//TODO: no funciona ningun tipo de metodo http, solo funciona el metodo GET en /api
+	r.Use(middlewares.Logging)
 
 	r.HandleFunc("/api", controllers.HandleHome).Methods("GET")
-	//r.HandleFunc("/api/notes", controllers.GetNotes).Methods("GET")
-	//r.HandleFunc("/api/notes/{id}", controllers.GetNoteByID).Methods("GET")
-	//r.HandleFunc("/api/notes/create", controllers.CreateNote).Methods("POST")
-	http.ListenAndServe(":3000", r)
+	r.HandleFunc("/api/notes", controllers.GetNotes).Methods("GET")
+	r.HandleFunc("/api/notes/{id}", controllers.GetNoteByID).Methods("GET")
+	r.HandleFunc("/api/notes/create", controllers.CreateNote).Methods("POST")
+	r.HandleFunc("/api/notes/{id}/update", controllers.UpdateNoteByID).Methods("PUT")
+	r.HandleFunc("/api/notes/{id}/delete", controllers.DeleteNoteByID).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":3000", r))
 }
