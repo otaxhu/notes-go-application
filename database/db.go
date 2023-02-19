56bb37b471662e06633d249153953f2db40ae692
oscar-jesus <oscarperniamoreno@gmail.com>
2023-02-19 00:26:18 -0400

    FindUserByID Method created

--- database/db.go
+++ database/db.go
@@ -1,7 +1,7 @@
 package database
 
 import (
-	"fmt"
+	"log"
 
 	"github.com/otaxhu/notes-go-application/environment"
 	"github.com/otaxhu/notes-go-application/models"
@@ -19,7 +19,7 @@ func ConnectAndAutoMigrate() {
 	// ya que se estaria creando otra instancia de &gorm.DB{}
 	NotesDB, err = gorm.Open(mysql.Open(environment.DSN), &gorm.Config{})
 	if err != nil {
-		fmt.Println(err.Error())
+		log.Println(err.Error())
 		return
 	}
 	NotesDB.AutoMigrate(&models.Note{})
@@ -29,6 +29,16 @@ func ConnectAndAutoMigrate() {
 func FindUserByEmail(email string) (*models.User, error) {
 	var user models.User
 	if err := NotesDB.First(&user, "email = ?", email).Error; err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &user, nil
+}
+func FindUserByID(id string) (*models.User, error) {
+	var user models.User
+	if err := NotesDB.First(&user, "id = ?", id).Error; err != nil {
 		if err == gorm.ErrRecordNotFound {
 			return nil, nil
 		}
