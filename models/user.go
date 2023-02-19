538f6370885d813ac9589650cab7d060b629e113
oscar-jesus <oscarperniamoreno@gmail.com>
2023-02-19 11:52:37 -0400

    define field tag type:varchar(255)

--- models/user.go
+++ models/user.go
@@ -11,6 +11,6 @@ type User struct {
 	CreatedAt time.Time      `json:"created_at"`
 	UpdatedAt time.Time      `json:"updated_at"`
 	DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted_at,omitempty"`
-	Email     string         `json:"email" gorm:"not null"`
-	Password  string         `json:"password" gorm:"not null"`
+	Email     string         `json:"email" gorm:"not null; type:varchar(255)"`
+	Password  string         `json:"password" gorm:"not null; type:varchar(255)"`
 }
