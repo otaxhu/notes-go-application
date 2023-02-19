fac147c1ac38ac0cbe519ccf84b8b243b60f0392
oscar-jesus <oscarperniamoreno@gmail.com>
2023-02-18 21:57:23 -0400

    environment imported

--- database/db.go
+++ database/db.go
@@ -3,6 +3,7 @@ package database
 import (
 	"fmt"
 
+	"github.com/otaxhu/notes-go-application/environment"
 	"github.com/otaxhu/notes-go-application/models"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -16,7 +17,7 @@ func ConnectAndAutoMigrate() {
 
 	// IMPORTANT! no se debe usar los dos puntos antes del "="
 	// ya que se estaria creando otra instancia de &gorm.DB{}
-	NotesDB, err = gorm.Open(mysql.Open(DSN), &gorm.Config{})
+	NotesDB, err = gorm.Open(mysql.Open(environment.DSN), &gorm.Config{})
 	if err != nil {
 		fmt.Println(err.Error())
 		return
