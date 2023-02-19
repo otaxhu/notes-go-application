958fd1fa15521f59a6b851736ff1cb04de43d0d8
oscar-jesus <oscarperniamoreno@gmail.com>
2023-02-18 21:56:29 -0400

    JWT functional

--- controllers/user.go
+++ controllers/user.go
@@ -2,42 +2,72 @@ package controllers
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
+	"time"
 
+	"github.com/golang-jwt/jwt"
 	"github.com/google/uuid"
 	"github.com/otaxhu/notes-go-application/database"
+	"github.com/otaxhu/notes-go-application/environment"
 	"github.com/otaxhu/notes-go-application/models"
 
 	"golang.org/x/crypto/bcrypt"
 )
 
-type UserResponse struct {
+type SignUpResponse struct {
 	ID    string `json:"id"`
 	Email string `json:"email"`
 }
+type LoginResponse struct {
+	Token string `json:"token"`
+}
 
-// TODO creacion de JWT para la autenticacion
 func LoginUser(w http.ResponseWriter, r *http.Request) {
+
 	var loginUser models.User
 	if err := json.NewDecoder(r.Body).Decode(&loginUser); err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
+
 	user, err := database.FindUserByEmail(loginUser.Email)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	if user == nil {
-		http.Error(w, "email or password are incorrect", http.StatusBadRequest)
+		http.Error(w, "email or password are incorrect", http.StatusUnauthorized)
 		return
 	}
+
 	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(loginUser.Password)); err != nil {
-		http.Error(w, "email or password are incorrect", http.StatusBadRequest)
+		http.Error(w, "email or password are incorrect", http.StatusUnauthorized)
 		return
 	}
-	fmt.Fprintf(w, "%s login succesfully", loginUser.Email)
+
+	claims := models.AppClaims{
+		UserID: loginUser.ID,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(48 * time.Hour).Unix(),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString([]byte(environment.JWTSecret))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	response := LoginResponse{
+		Token: tokenString,
+	}
+
+	w.Header().Set("content-type", "application/json")
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
 
 func CreateUser(w http.ResponseWriter, r *http.Request) {
@@ -82,7 +112,7 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	response := UserResponse{
+	response := SignUpResponse{
 		ID:    newUser.ID,
 		Email: newUser.Email,
 	}
