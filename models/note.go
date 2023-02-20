3cb145b96f29bb3d13c8118caa00043f612fa7e8
oscar-jesus <oscarperniamoreno@gmail.com>
2023-02-20 14:27:23 -0400

    foreignKey configured

--- models/note.go
+++ models/note.go
@@ -7,10 +7,12 @@ import (
 )
 
 type Note struct {
-	ID          string         `gorm:"primaryKey" json:"id"`
+	ID          string         `gorm:"primaryKey;type:varchar(255)" json:"id"`
 	CreatedAt   time.Time      `json:"created_at"`
 	UpdatedAt   time.Time      `json:"updated_at"`
 	DeletedAt   gorm.DeletedAt `gorm:"index" json:"deleted_at,omitempty"`
-	Title       string         `json:"title" gorm:"not null"`
-	Description string         `json:"description" gorm:"not null"`
+	Title       string         `json:"title" gorm:"not null;type:varchar(100)"`
+	Description string         `json:"description" gorm:"not null;type:varchar(255)"`
+	UserID      string         `json:"user_id" gorm:"not null"`
+	User        User           `json:"user"`
 }
--- models/user.go
+++ models/user.go
@@ -7,10 +7,11 @@ import (
 )
 
 type User struct {
-	ID        string         `gorm:"primaryKey" json:"id"`
+	ID        string         `gorm:"primaryKey;type:varchar(255)" json:"id"`
 	CreatedAt time.Time      `json:"created_at"`
 	UpdatedAt time.Time      `json:"updated_at"`
 	DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted_at,omitempty"`
 	Email     string         `json:"email" gorm:"not null; type:varchar(255)"`
 	Password  string         `json:"password" gorm:"not null; type:varchar(255)"`
+	Notes     []Note         `json:"notes" gorm:"foreignKey:user_id"`
 }
